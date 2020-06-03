f7bb219425b41bee600da0e4642ae4ca3f49a799
tanguyplayer <bonneau.tanguy1@gmail.com>
2020-06-03 11:47:12 +0200

    modification non fonctionnel

--- pageModifTemp.php
+++ pageModifTemp.php
@@ -1,23 +1,38 @@
 <html>  
     <head>  
-    <title>Dynamic Drop Down List</title>  
+    <title>Modifier produit</title>  
     </head>  
     <body>  
         <form id="form1" name="form1" method="post">
 			<?php
 				require("fonctionsSQL.php");
 				$connexion = connexion ();
-				$request = $connexion->query("SELECT id_produit, nomProduit from produit;");
+				$request = $connexion->query("SELECT * from produit;");
 				echo '<select name="nomProduit">';
 					while ($donnees = $request->fetch())
 					{
-						echo hdqfbuswdf;
 						echo '<option value="'.$donnees['id_produit'].'">'.$donnees['nomProduit'].'</option>';
 						
 					}
 				echo '</select>';
 			?>
             <input type="submit" name="Submit" value="Select" />  
-        </form>  
+			</br>
+			<label>Nom du produit</label>
+			<input type="text" class="form-control" name="nomProduit" value="" required>
+			</br>
+			<label>Tarif achat HT</label>
+			<input type="text" class="form-control" name="tarifHT" value=""required>
+			</br>
+			<label>Tarif revente TTC</label>
+			<input type="text" class="form-control" name="tarifTTC" value="">
+			</br>
+			<label>Fournisseur</label>
+			<input type="text" class="form-control" name="fournisseur" value="">	
+			</br>
+			<label >Stock</label>
+			<input type="text" class="form-control" name="stock" value="">
+			</br>
+        </form> 
     </body>  
 </html>  
